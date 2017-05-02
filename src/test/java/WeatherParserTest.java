e8a38e4ab3a6736dc77a48c2ee9f5c9867fed60e
dert1714 <chuvashkinvk@gmail.com>
2017-05-02 21:42:24 +0400

    3-WeatherParser-unitTest

--- src/test/java/WeatherParserTest.java
+++ src/test/java/WeatherParserTest.java
@@ -12,11 +12,13 @@ public class WeatherParserTest {
         Map<String, Map<String, Integer>> map = w.getData("src/main/resources/test1.json");
         Map<String, Map<String, Integer>> real_map = new HashMap<>();
         Map<String, Integer> tmp_map = new HashMap<>();
+        Map<String, Integer> tmp_map1 = new HashMap<>();
         tmp_map.put("temperature_min", 0);
         tmp_map.put("temperature_max", 30);
         real_map.put("April", tmp_map);
-        real_map.put("May", tmp_map);
+        tmp_map1.put("temperature_min", 10);
+        tmp_map1.put("temperature_max", 20);
+        real_map.put("May", tmp_map1);
         Assert.assertEquals(real_map, map);
     }
-
 }
